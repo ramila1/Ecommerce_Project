4058372362da87722f331f5f8eadd6f51cebf4d1
Ramila <ramilashahi2799@gmail.com>
2024-10-22 07:56:56 +0545

    user model created

---
+++ server/models/user.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'Name is required']
+    },
+    email:{
+        type:String,
+        required:[true,'Email is required'],
+        unique:[true,'Email already exists']
+    },
+    password:{
+        type:String,
+        required:[true,'Password is required'],
+        minlength:[6,'Password length should be greater than 6']
+    },
+    address:{
+        type:String,
+        require:[true,'Address is required']
+    },
+    city:{
+        type:String,
+        required:[true,'City name is required']
+    },
+    country:{
+        type:String,
+        require:[true,'Country name is required']
+    },
+    phone:{
+        type:String,
+        require:[true,'Phone number is required']
+    },
+    profilePicture:{
+        type:String
+    }
+
+},{timestamps:true});
+
+export const User = mongoose.model("Users",userSchema);
